4b7010d08d02a8d17a8cdbc613f52f701c35b479
soniatamx <soniatamx@gmail.com>
2017-02-26 21:22:26 -0500

    modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,2 @@
 var stickyNav = new Foundation.Sticky($('#sticky-navigation').foundation(), {marginTop: 0, stickTo: 'top', stickyOn:'small'});
+var modal = new Foundation.Reveal($('#exampleModal1').foundation())
