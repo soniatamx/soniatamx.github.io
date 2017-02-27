12ce9cb4fbf90c5b7cb585ff2be47992c3413541
soniatamx <soniatamx@gmail.com>
2017-02-27 00:49:56 -0500

    gallery and modal fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,2 @@
 var stickyNav = new Foundation.Sticky($('#sticky-navigation').foundation(), {marginTop: 0, stickTo: 'top', stickyOn:'small'});
-var modal = new Foundation.Reveal($('#exampleModal1').foundation())
+$('#exampleModal1').foundation();
