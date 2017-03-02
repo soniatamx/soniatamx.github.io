885d74926deb893343dd1e2f5721367950998ba1
Sonia <soniatamx@gmail.com>
2017-03-02 17:27:14 -0500

    modal js loop

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var stickyNav = new Foundation.Sticky($('#sticky-navigation').foundation(), {marginTop: 0, stickTo: 'top', stickyOn:'small'});
-$('#galleryModal').foundation();
-$('#galleryModal2').foundation();
+function instantiateModals() {
+
+  var modalsChecked = false;
+  var modalCounter = 0;
+
+  while(!modalsChecked) {
+    if(document.getElementById('galleryModal-' + (modalCounter + 1))) {
+      modalCounter++;
+    } else {
+      modalsChecked = true;
+    }
+  }
+
+  var modalList = []
+  for(var i = 0; i < modalCounter; i++) {
+    modalList.push('#galleryModal-' + (i + 1));
+  }
+
+  $(modalList.join(',')).foundation();
+
+}
+
+instantiateModals();
 
 },{}]},{},[1]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
 var stickyNav = new Foundation.Sticky($('#sticky-navigation').foundation(), {marginTop: 0, stickTo: 'top', stickyOn:'small'});
-$('#galleryModal').foundation();
-$('#galleryModal2').foundation();
+function instantiateModals() {
+
+  var modalsChecked = false;
+  var modalCounter = 0;
+
+  while(!modalsChecked) {
+    if(document.getElementById('galleryModal-' + (modalCounter + 1))) {
+      modalCounter++;
+    } else {
+      modalsChecked = true;
+    }
+  }
+
+  var modalList = []
+  for(var i = 0; i < modalCounter; i++) {
+    modalList.push('#galleryModal-' + (i + 1));
+  }
+
+  $(modalList.join(',')).foundation();
+
+}
+
+instantiateModals();
