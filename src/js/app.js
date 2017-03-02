66165b3a29f07ed732870e56dea0bb382d8af5be
Sonia <soniatamx@gmail.com>
2017-03-02 15:13:36 -0500

    more modal port

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,3 @@
 var stickyNav = new Foundation.Sticky($('#sticky-navigation').foundation(), {marginTop: 0, stickTo: 'top', stickyOn:'small'});
 $('#galleryModal').foundation();
+$('#galleryModal2').foundation();
